c30a37fe0e15821829ffbb5e3f3893168e2177a5
nguyenson-toray <son.nt@tiqn.com.vn>
2025-09-19 09:17:05 +0700

    Employee Checkin : not allow change values of exits record

--- customize_erpnext/public/js/custom_scripts/employee_checkin.js
+++ customize_erpnext/public/js/custom_scripts/employee_checkin.js
@@ -1,7 +1,7 @@
 frappe.ui.form.on("Employee Checkin", {
 	refresh: async (frm) => {
 		if (frm.doc.__islocal) {
-			// set custom_reason_for_manual_check_in is madotory if add "Employee Checkin"  manually
+			// set custom_reason_for_manual_check_in is mandotory if add "Employee Checkin"  manually
 			frm.set_df_property(
 				"custom_reason_for_manual_check_in",
 				"reqd",
@@ -18,9 +18,12 @@ frappe.ui.form.on("Employee Checkin", {
 					"Other"
 				]
 			);
-			// set skip_auto_attendance hidden if add "Employee Checkin"  manually 
 			frm.set_df_property("skip_auto_attendance", "hidden", 1);
 			frm.set_df_property("log_type", "hidden", 1);
+			frm.set_df_property("employee", "read_only", 0);
+			frm.set_df_property("time", "read_only", 0);
+			frm.set_df_property("device_id", "read_only", 1);
+			frm.set_df_property("custom_reason_for_manual_check_in", "read_only", 0);
 		}
 	},
 
