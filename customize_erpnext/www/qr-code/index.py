cfb48ff7e1fb19b54e94f3932ec5c80525682778
nguyenson-toray <son.nt@tiqn.com.vn>
2026-02-27 16:52:16 +0700

    * QR Code Generator page : public page, no login required

--- customize_erpnext/www/qr-code/index.py
+++ customize_erpnext/www/qr-code/index.py
@@ -2,9 +2,10 @@ import frappe
 
 
 def get_context(context):
-	if frappe.session.user == "Guest":
-		frappe.local.flags.redirect_location = "/login?redirect-to=/qr-code"
-		raise frappe.Redirect
+	# public page, no login required
+	# if frappe.session.user == "Guest":
+	# 	frappe.local.flags.redirect_location = "/login?redirect-to=/qr-code"
+	# 	raise frappe.Redirect
 
 	context.no_cache = 1
 	return context
